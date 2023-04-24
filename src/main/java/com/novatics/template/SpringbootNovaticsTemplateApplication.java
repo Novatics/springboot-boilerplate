f169cabef0b0cb64464bd713681fb8b39436ee68
fjansen-appen <fjansen@appen.com>
2023-04-24 19:57:56 -0300

    Initial template

---
+++ src/main/java/com/novatics/template/SpringbootNovaticsTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.novatics.template;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNovaticsTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNovaticsTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/novatics/template/SpringbootNovaticsTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novatics.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootNovaticsTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
